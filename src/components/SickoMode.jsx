effe6b66ba404f6b80cf50469fd8f388c7a6eed8
gunnar <gunnar1hoglund@gmail.com>
2019-09-18 22:02:38 -0700

    first commit

---
+++ src/components/SickoMode.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class SickoMode extends React.Component {
+  render() {
+    return (
+      <div className="album">
+        <img />
+        <p>playlist name</p>
+      </div>
+    );
+  }
+}
+
+export default SickoMode;
